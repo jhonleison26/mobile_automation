68cdbc0c0fe4fa6c79b79f85b0dcaab2a42d8955
Jhon olivares <jhon.olivares@globant.com>
2025-09-15 00:38:33 -0500

    enhance BaseScreen with element interaction methods and visibility check

--- src/test/java/org/jhon/mobile/utils/BaseScreen.java
+++ src/test/java/org/jhon/mobile/utils/BaseScreen.java
@@ -1,11 +1,42 @@
 package org.jhon.mobile.utils;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BaseScreen {
     protected AppiumDriver driver;
 
     public BaseScreen(AppiumDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public void sendKeys(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+
+    public boolean isTheElementVisible(WebElement element, int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+
+        try {
+            WebElement element1 = wait.until(ExpectedConditions.visibilityOf(element));
+            return element != null;
+        } catch (NoSuchElementException e) {
+            return false;
+        } finally {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        }
     }
 }
