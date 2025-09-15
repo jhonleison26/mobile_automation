39915316fee15bc869deeab0d03076811767ed1a
Jhon olivares <jhon.olivares@globant.com>
2025-09-15 09:59:57 -0500

    add SignUpScreen and SignUpTest classes with signup functionality and validation

---
+++ src/test/java/org/jhon/mobile/tests/SignUpTest.java
@@ -0,0 +1,39 @@
+package org.jhon.mobile.tests;
+
+import org.jhon.mobile.screens.HomeScreen;
+import org.jhon.mobile.screens.LoginScreen;
+import org.jhon.mobile.screens.SignUpScreen;
+import org.jhon.mobile.utils.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+public class SignUpTest extends BaseTest {
+
+    @Test
+    public void testSuccessfulSignUp() {
+        // Go Home
+        HomeScreen homeScreen = new HomeScreen(driver);
+
+        // Navigate to Login
+        homeScreen.goToLogin();
+        LoginScreen loginScreen = new LoginScreen(driver);
+        Assert.assertTrue(loginScreen.isLoginScreenVisible(), "Login screen is not visible");
+
+        // Go SignUp
+        loginScreen.goToSignUp();
+        SignUpScreen signUpScreen = new SignUpScreen(driver);
+
+        // Generate random email and password
+        String randomEmail = "test_" + UUID.randomUUID().toString().substring(0, 5) + "@mail.com";
+        String password = "Password123";
+
+        // Perform SignUp
+        signUpScreen.doSignup(randomEmail, password);
+
+        // Validate SignUp success
+        Assert.assertTrue(signUpScreen.isSignupSuccessful(), "Sign Up was not successful");
+    }
+}
+
